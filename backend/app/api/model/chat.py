dd159768ab0378ffb27f196811025754ab4de635
viraj <virajpatwardhan6@gmail.com>
2026-01-01 16:10:34 +0530

    feat: updated models

--- backend/app/api/model/chat.py
+++ backend/app/api/model/chat.py
@@ -1,10 +1,26 @@
-from sqlalchemy import String, DateTime, Column, UUID, ForeignKey, func
+from sqlalchemy import String, DateTime, Column, ForeignKey, func
+from sqlalchemy.dialects.postgresql import UUID
 from api.database.db import Base
 import uuid
 # from sqlalchemy.dialects.postgresql import UUID
 
 class Chat(Base):
+    __tablename__ = "chats"
+    
     id = Column(UUID(as_uuid=True), primary_key=True, unique=True, default=uuid.uuid4)
-    connection_id = Column(String, ForeignKey('personna.connection_id', ondelete='CASCADE'), nullable=False)
+    connection_id = Column(String)
+    user_id = Column(UUID(as_uuid=True), ForeignKey('users.id', ondelete='CASCADE'))
+    role = Column(String, nullable=False)  # "user" | "ai" | "system"
     content = Column(String, nullable=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+    created_at = Column(
+        DateTime(timezone=True),
+        server_default=func.now()
+    )
+
+    updated_at = Column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        onupdate=func.now()
+    )
--- backend/app/api/model/personna.py
+++ backend/app/api/model/personna.py
@@ -1,13 +1,23 @@
-from sqlalchemy import String, func, DateTime, Column
+from sqlalchemy import String, func, DateTime, Column, ForeignKey
 import uuid
 from sqlalchemy.dialects.postgresql import UUID
 from api.database.db import Base
 
-class Personna(Base):
-    __tablename__ =  'personna'
+class Persona(Base):
+    __tablename__ =  'persona'
     
     id = Column(UUID(as_uuid=True), primary_key=True, unique=True, index=True, default=uuid.uuid4)
-    connection_id = Column(String) # user Id and persona Id
-    user_id = Column(String)
-    personna_id = Column(String)
-    personna_name = Column(String)
+    # connection_id = Column(String) # user Id + persona Id
+    user_id = Column(UUID(as_uuid=True), ForeignKey("users.id"), nullable=False)
+    persona_name = Column(String, nullable=False)
+    
+    created_at = Column(
+        DateTime(timezone=True),
+        server_default=func.now()
+    )
+
+    updated_at = Column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        onupdate=func.now()
+    )
