dd63ac8107ae39997d7e94f1017b21e7ac47cdc5
viraj <virajpatwardhan6@gmail.com>
2026-01-08 12:36:22 +0530

    feat: landing page animations complete

--- frontend/app/components/Hero.tsx
+++ frontend/app/components/Hero.tsx
@@ -1,15 +1,44 @@
 "use client";
 import { FaArrowAltCircleUp } from "react-icons/fa";
 import { useEffect, useRef, useState } from "react";
+import Navbar from "./Navbar";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 const Hero = () => {
   const [isMobile, setIsMobile] = useState<boolean>();
-  const [transform, setTransform] = useState(0);
-  const imgRef = useRef(null)
+  const [transform, setTransform] = useState("");
+  const [transformRight, setTransformRight] = useState("");
+  const imgRef = useRef<HTMLDivElement>(null);
+  const imgRef2 = useRef<HTMLDivElement>(null);
+  const videoRef = useRef<HTMLDivElement>(null);
 
-  const onMouseMoveHandler = () => {
+  const onMouseMoveHandler = (e: React.MouseEvent, type: "left" | "right") => {
+    if (!imgRef.current) return;
 
-  }
+    const { top, left, width } = imgRef.current.getBoundingClientRect();
+    const relativeX = (e.clientX - left) / width;
+    const relativeY = (e.clientY - top) / width;
+
+    if (type === "left") {
+      const tiltX = (relativeX - 0.5) * 15;
+      const tiltY = (relativeY - 0.5) * -15;
+
+      const newTransform = `perspective(700px) rotateX(${tiltX}deg) rotateY(${tiltY}deg) scale3d(0.98, 0.98, 0.98)`;
+      setTransform(newTransform);
+    } else {
+      const tiltX = (relativeX - 0.5) * 2;
+      const tiltY = (relativeY - 0.5) * -2;
+
+      const newTransform = `perspective(700px) rotateX(${tiltX}deg) rotateY(${tiltY}deg) scale3d(0.98, 0.98, 0.98)`;
+      setTransformRight(newTransform);
+    }
+  };
+
+  const onMouseLeaveHandler = () => {
+    setTransform("");
+    setTransformRight("");
+  };
 
   useEffect(() => {
     const handleResize = () => setIsMobile(window.innerWidth < 640);
@@ -17,34 +46,62 @@ const Hero = () => {
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
   }, []);
+
+  useGSAP(() => {
+    const timeline = gsap.timeline();
+    timeline
+      .to(videoRef.current, {
+        y: 0,
+        ease: "sine.inOut",
+        duration: 0.6,
+      })
+      .to("#hero-section video", {
+        opacity: 1,
+        ease: "sine.inOut",
+      });
+
+    gsap.to("#text", {
+      opacity: 1,
+      ease: "sine.inOut",
+      duration: 0.6,
+    });
+  });
   return (
-    <div className="relative text-black">
-      <section id="hero-section" className="">
-        <p className="bg-white inline px-3 py-1 rounded-xl">
+    <div className="relative text-black max-w-5xl mx-auto">
+      <Navbar />
+      <section id="hero-section">
+        {/* Navbar */}
+        <p id="text" className="bg-white inline px-3 py-1 rounded-xl opacity-0">
           Talk to personas and chat with them
         </p>
-        <h1>Your Creative Partner, For Conversations</h1>
-        <p className="max-w-lg mx-auto">
+        <h1 id="text" className="opacity-0">
+          Your Creative Partner, For Conversations
+        </h1>
+        <p id="text" className="max-w-lg mx-auto opacity-0">
           From dynamic video generation to intelligent chat, our AI-powered
           suite helps you create compelling content and conversations. <br />
           Your all-in-one Solution for modern creation
         </p>
-
+        {/* Video */}
         <video
           src={"/gradient.mp4"}
-          className="absolute bottom-0 bg-red-500 w-full rounded-tl-2xl rounded-tr-2xl h-28 sm:h-40 object-cover z-20"
+          className="absolute bottom-0 bg-red-500 w-full rounded-tl-2xl rounded-tr-2xl h-28 sm:h-40 object-cover z-20 opacity-0"
           autoPlay
           loop
         ></video>
+        {/* TextArea */}
         <div
-          className="absolute bottom-0 bg-gray-100 rounded-tl-2xl rounded-tr-2xl h-24! sm:h-36! box-border! z-30 left-1/2 -translate-x-1/2"
+          className="absolute bottom-0 bg-gray-100 rounded-tl-2xl rounded-tr-2xl h-24! sm:h-36! box-border! z-30 left-1/2 -translate-x-1/2 
+          translate-y-4"
           style={{ width: "calc(100% - 1.5rem)" }}
+          ref={videoRef}
         >
-          <form action="" className="">
+          <form action="">
             <textarea
               // type="text"
               placeholder="Ask your question to Warren Buffet's Persona"
               className="mt-2 w-full px-4 outline-none resize-none"
+              disabled
               rows={isMobile ? 3 : 2}
             />
           </form>
@@ -55,22 +112,38 @@ const Hero = () => {
             </button>
           </div>
         </div>
-        <div className="rounded-lg absolute top-[52%] xl:-translate-x-1/2 translate-x-1/2 -translate-y-1/2 lg:top-[55%]">
+        {/* Images */}
+        <div
+          onMouseMove={(e) => {
+            onMouseMoveHandler(e, "left");
+          }}
+          onMouseLeave={onMouseLeaveHandler}
+          ref={imgRef}
+          style={{ transform: transform }}
+          className="rounded-lg absolute top-[52%] xl:-translate-x-1/2 translate-x-1/2 -translate-y-1/2 lg:top-[55%] transition-all ease-in duration-75 max-md:top-[65%]"
+        >
           <img
             src="/newyork.jpg"
             alt="new york"
-            className="h-80 w-72 rounded-3xl"
-              style={{ clipPath: "polygon(28% 5%, 95% 5%, 72% 95%, 5% 95%)" }}
+            className="h-80 w-72 rounded-3xl shadow-lg opacity-0"
+            // style={{ clipPath: "polygon(28% 5%, 95% 5%, 72% 95%, 5% 95%)" }}
           />
         </div>
         <div
           className="absolute lg:top-[10%] xl:top-0 right-0 xl:translate-x-1/2
-        -translate-x-1/2  lg:translate-y-[80%] "
+        -translate-x-1/2  lg:translate-y-[80%] transition-all ease-in duration-75 max-md:top-[40%]"
+          onMouseMove={(e) => {
+            onMouseMoveHandler(e, "right");
+          }}
+          onMouseLeave={onMouseLeaveHandler}
+          ref={imgRef2}
+          style={{ transform: transformRight }}
         >
           <img
             src="/london.jpg"
             alt="london"
-            className="h-72 w-52 rounded-lg"
+            className="h-72 w-52 rounded-lg shadow-lg opacity-0"
+            // style={{ clipPath: "polygon(0 0, 80% 0, 100% 100%, 12% 100%)" }}
           />
         </div>
       </section>
---
+++ frontend/app/components/Navbar.tsx
@@ -0,0 +1,152 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+const Navbar = () => {
+  //   useGSAP(() => {
+  //     // const timeline = gsap.timeline({
+  //     //     scrollTrigger: {
+  //     //         trigger: "#navbar-section",
+  //     //         start: "top top",
+  //     //         end: "top top",
+  //     //         markers: true,
+  //     //         scrub: 1,
+  //     //         pin: true
+  //     //     }
+  //     // })
+
+  //     // timeline.to("img", {
+  //     //     opacity: 0,
+  //     //     x: 10,
+  //     //     ease: "sine.inOut"
+  //     // })
+  //     const timeline = gsap.timeline();
+  //     // gsap.set(["img", "h3", ".nav-links", ".nav-buttons"], {
+  //     //   opacity: 0,
+  //     //   x: -20,
+  //     // });
+
+  //     // Animate all at once with stagger
+  //     // timeline.to(["#navbar-section", "img", "nav-links", "li", ".nav-buttons"], {
+  //     //   opacity: 1,
+  //     //   x: 0,
+  //     //   y:0,
+  //     //   duration: 0.4,
+  //     //   ease: "sine.inOut",
+  //     //   stagger: 0.2, // 0.2s delay between each element
+  //     // });
+  //     timeline.to("#navbar-section img", {
+  //       opacity: 1,
+  //       x: 0,
+  //       duration: 0.2,
+  //       ease: "sine.inOut",
+  //     });
+  //   });
+
+  useGSAP(() => {
+    const timeline = gsap.timeline();
+
+    // Start with elements invisible
+    gsap.set(
+      [
+        "img",
+        "h3",
+        "#nav-links",
+        "#nav-buttons",
+        "#navbar-section",
+        "#links-bg",
+      ],
+      {
+        opacity: 0,
+      }
+    );
+
+    // Animate in sequence
+    timeline
+      .to("#navbar-section", {
+        opacity: 1,
+        y: 0,
+        x: 0,
+        duration: 0.6,
+        ease: "sine.inOut",
+      })
+      .to("img", {
+        opacity: 1,
+        x: 0,
+        duration: 0.6,
+        ease: "sine.inOut",
+      })
+      .to(
+        "#navbar-section h3",
+        {
+          opacity: 1,
+          x: 0,
+          duration: 0.6,
+          ease: "sine.inOut",
+        },
+        "-=0.3"
+      )
+      .to(
+        "#links-bg",
+        {
+          opacity: 1,
+          duration: 0.3,
+          ease: "sine.inOut",
+        },
+        "-=0.3"
+      )
+      .to("#nav-links", {
+        opacity: 1,
+        scale: 1,
+        duration: 0.3,
+        ease: "sine.inOut",
+        stagger: 1,
+      })
+      .to(
+        "#nav-buttons",
+        {
+          opacity: 1,
+          duration: 0.3,
+          delay: 0.2,
+          ease: "sine.inOut",
+          stagger: 1,
+        },
+        "-=0.3"
+      );
+  }, []);
+  return (
+    <div
+      id="navbar-section"
+      className="flex-between z-100 -translate-y-2 opacity-0"
+    >
+      <div className="flex items-center gap-4">
+        <img
+          src="/london.jpg"
+          className="h-14 w-14 rounded-full -translate-x-2"
+        />
+        <h3 className="text-lg font-black uppercase">Persona AI</h3>
+      </div>
+      <div
+        id="links-bg"
+        className="bg-gray-100 px-10 py-2 rounded-full hidden md:block"
+      >
+        <ul
+          id="nav-links"
+          className="flex-center gap-4 font-semibold scale-110"
+        >
+          <li>Features</li>
+          <li>Pricing</li>
+          <li>Blogs</li>
+        </ul>
+      </div>
+      <div id="nav-buttons" className="flex gap-4 font-semibold">
+        <button className="secondary-button cursor-pointer">Login</button>
+        <button className="primary-button bg-black cursor-pointer text-white">
+          Sign up
+        </button>
+      </div>
+    </div>
+  );
+};
+export default Navbar;
